13c5713f33b6ff07ec6d90b38c7d2dc7c88851cd
xarhsdev <xarhsdev@outlook.com>
2017-07-20 12:30:06 +0300

    More implementation

---
+++ Source/Epignosis/Client/Failure/Http.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Epignosis\Client\Failure;
+
+/**
+ * Class Http
+ *
+ * The http client exception.
+ *
+ * @author      Haris Batsis <xarhsdev@efrontlearning.com>
+ * @category    Epignosis\Client\Failure
+ * @copyright   Epignosis LLC (c) Copyright 2017, All Rights Reserved
+ * @package     Epignosis\Client\Failure
+ * @since       1.0.0-dev
+ */
+class Http extends Client
+{
+
+}
--- Source/Epignosis/Client/Http.php
+++ Source/Epignosis/Client/Http.php
@@ -3,8 +3,19 @@
 namespace Epignosis\Client;
 
 use Epignosis\Client\Abstraction\ClientInterface;
+use Epignosis\Client\Failure\Http as HttpClientException;
 
-
+/**
+ * Class Http
+ *
+ * The HTTP client.
+ *
+ * @author      Haris Batsis <xarhsdev@efrontlearning.com>
+ * @category    Epignosis\Client
+ * @copyright   Epignosis LLC (c) Copyright 2017, All Rights Reserved
+ * @package     Epignosis\Client
+ * @since       1.0.0-dev
+ */
 class Http implements ClientInterface
 {
   /**
@@ -20,13 +31,13 @@ class Http implements ClientInterface
    *
    * @since   1.0.0-dev
    *
-   * @throws  ClientException
+   * @throws  HttpClientException
    *            - In case that is not possible to successfully complete, the creation
    *              operation.
    */
   public function Create(array $configuration, array $data = [])
   {
-
+    return [];
   }
 
   /**
@@ -42,13 +53,13 @@ class Http implements ClientInterface
    *
    * @since   1.0.0-dev
    *
-   * @throws  ClientException
+   * @throws  HttpClientException
    *            - In case that is not possible to successfully complete, the delete
    *              operation.
    */
   public function Delete(array $configuration, array $data = [])
   {
-
+    return [];
   }
 
   /**
@@ -64,13 +75,13 @@ class Http implements ClientInterface
    *
    * @since   1.0.0-dev
    *
-   * @throws  ClientException
+   * @throws  HttpClientException
    *            - In case that is not possible to successfully complete, the retrieval
    *              operation.
    */
   public function Retrieve(array $configuration, array $data = [])
   {
-
+    return [];
   }
 
   /**
@@ -86,12 +97,12 @@ class Http implements ClientInterface
    *
    * @since   1.0.0-dev
    *
-   * @throws  ClientException
+   * @throws  HttpClientException
    *            - In case that is not possible to successfully complete, the update
    *              operation.
    */
   public function Update(array $configuration, array $data = [])
   {
-
+    return [];
   }
 }
