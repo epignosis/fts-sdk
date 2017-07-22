777ddfff188ba28f421730544caa902ead355da6
xarhsdev <xarhsdev@outlook.com>
2017-07-22 15:09:40 +0300

    More implementation

--- Demo/Data/Document/Create.php
+++ Demo/Data/Document/Create.php
@@ -1,12 +1,18 @@
 <?php
 
+/** @noinspection SpellCheckingInspection */
 $data = [
 
   /**
    * Document #1
    */
   [
-    'UnitId' => 1,
+    'Id' => [
+      'Unit' => 1,
+      'Course' => 13,
+      'Lesson' => 20
+    ],
+
     'Title' => '(1.0) Introduction to Programming',
 
     'RequirementList' => [
@@ -22,7 +28,11 @@ $data = [
 
     'Content' => [
 
-      'UrlList' => [],
+      'UrlList' => [
+        'http://math.hws.edu/eck/cs124/downloads/javanotes6-linked.pdf',
+        'http://cevre.beun.edu.tr/zeydan/pdf/introduction-to-computer-programming.pdf',
+        'http://www.icsd.aegean.gr/lecturers/kavallieratou/Cplusplus_files/notes.pdf'
+      ],
       'Text' =>
         'A program is a set of instructions that tell the computer to do various ' .
         'things; sometimes the instruction it has to perform depends on what happened ' .
