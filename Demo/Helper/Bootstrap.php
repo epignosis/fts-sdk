266d10933dd3b61ddf0234921585167b61d0215e
xarhsdev <xarhsdev@outlook.com>
2017-07-22 21:14:43 +0300

    More implementation

--- Demo/Helper/Bootstrap.php
+++ Demo/Helper/Bootstrap.php
@@ -7,11 +7,31 @@ use Epignosis\Helper\AutoLoader\AutoLoader;
 $_SERVER['DEMO_NOW'] = microtime(true);
 date_default_timezone_set('UTC');
 
-if (false !== stripos(php_sapi_name(), 'cli', 0) || isset($_SERVER['argv'])) {
+if (false !== stripos(php_sapi_name(), 'cli', 0) || !empty($_SERVER['argv'])) {
   echo 'Command line interface (CLI) is not allowed. Script execution was terminated.';
 
   exit(1);
 }
+
+register_shutdown_function(function() {
+  $error = error_get_last();
+
+  $errorTypeList = [
+    \E_COMPILE_ERROR,
+    \E_CORE_ERROR,
+    \E_ERROR,
+    \E_RECOVERABLE_ERROR,
+    \E_USER_ERROR
+  ];
+
+  if (in_array($error['type'], $errorTypeList, true)) {
+    echo sprintf('<pre>%s</pre>', print_r($error, true));
+
+    exit(2);
+  }
+
+  exit(0);
+});
 
 require 'Printer.php';
 
