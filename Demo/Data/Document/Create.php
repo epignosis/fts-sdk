e5ad79477a6f1fdf9363d8ad0ea80551885a3b6a
xarhsdev <xarhsdev@outlook.com>
2017-07-23 11:20:27 +0300

    More implementation

--- Demo/Data/Document/Create.php
+++ Demo/Data/Document/Create.php
@@ -7,6 +7,9 @@ $data = [
    * Document #1
    */
   [
+
+    'Title' => '(1.0) Introduction to Programming',
+
     'Id' => [
       'Unit' => 1,
       'Course' => 13,
@@ -14,32 +17,40 @@ $data = [
       'Domain' => null
     ],
 
-    'Title' => '(1.0) Introduction to Programming',
-
     'RequirementList' => [
 
       'Availability' => [
         'From' => null,
         'To' => null
       ],
+
       'CourseList' => [],
       'Unit' => null
 
     ],
 
+    'Order' => [
+      'Custom' => 6,
+      'Product' => 17
+    ],
+
     'Content' => [
 
-      'UrlList' => [
+      'FileList' => [
         'http://math.hws.edu/eck/cs124/downloads/javanotes6-linked.pdf',
         'http://cevre.beun.edu.tr/zeydan/pdf/introduction-to-computer-programming.pdf',
         'http://www.icsd.aegean.gr/lecturers/kavallieratou/Cplusplus_files/notes.pdf'
       ],
+
       'Text' =>
         'A program is a set of instructions that tell the computer to do various ' .
         'things; sometimes the instruction it has to perform depends on what happened ' .
-        'when it performed a previous instruction.'
+        'when it performed a previous instruction.',
+
+      'TagList' => []
 
     ]
+
   ]
 
 ];
