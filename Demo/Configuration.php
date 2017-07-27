ca07810ee45a78e55075a3df0099f8c3f68f7a32
xarhsdev <xarhsdev@outlook.com>
2017-07-27 13:07:08 +0300

    Development

--- Demo/Configuration.php
+++ Demo/Configuration.php
@@ -1,21 +1,24 @@
 <?php
 
+/**
+ * NOTE: Limited/Demo Keys!
+ */
 $configuration = [
 
   'Auth' => [
 
     'Key' => [
 
-      // Length: 32; Format: HEX
+      // Length: 64; Format: Base64
       'Private' => [
-        'Read' => 'Demo',
-        'Write' => 'Demo'
+        'Read' => 'pcSEcve89u2+nkLuQIbvQHenZDTL0oCWGsJePg58E0WB7jbZnGsTIkXF+hUY4ih6',
+        'Write' => 'gKNp4KXTRrDlA2jOj+vWIk0kT4vJh7MdMCqM7dUuOSSjnOqHvJ66TNXllxuE3ycV'
       ],
 
       // Length: 64; Format: Base64
       'Public' => [
-        'Read' => 'Demo',
-        'Write' => 'Demo'
+        'Read' => 'HFM/n0oSe8jqdfm6vLPB0PlAW1tFBmaU3FWjrLYOycMyw8rOijm960CvJOkSDtMB',
+        'Write' => 'GmVeZ6hfe1P+KEo63xA+kwMKgu0XW6kWGwyxCVAGpM2pG3Rmw/nirfRsYys3EtJc'
       ]
 
     ]
