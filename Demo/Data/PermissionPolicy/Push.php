151925783c714103d7c98049efe52ee9cb5d7f59
xarhsdev <xarhsdev@outlook.com>
2017-08-03 13:56:37 +0300

    Development

---
+++ Demo/Data/PermissionPolicy/Push.php
@@ -0,0 +1,102 @@
+<?php
+
+/** @noinspection SpellCheckingInspection */
+$data = [
+
+  /**
+   * Permission Policy #1
+   * ------------------------------------------------------------------------------------
+   */
+  [
+
+    /**
+     * @default       -
+     * @description   The permission policy data. It is an indexed array, with accessible
+     *                course identifiers as keys, and an array of non-accessible unit
+     *                identifiers as values. MUST be in JSON format.
+     *
+     *                Example: {"7":[],"22":[6,9]}
+     *
+     * @required      true
+     * @since         1.0.0-dev
+     * @var           bool
+     */
+    'Data' => '{"39":[5]}',
+
+    /**
+     * @default       -
+     * @description   The permission policy identifier. Could be the internal user
+     *                identifier on the LMS.
+     * @required      true
+     * @since         1.0.0-dev
+     * @var           int
+     */
+    'Id' => 1
+
+  ],
+
+  /**
+   * Permission Policy #2
+   * ------------------------------------------------------------------------------------
+   */
+  [
+
+    /**
+     * @default       -
+     * @description   The permission policy data. It is an indexed array, with accessible
+     *                course identifiers as keys, and an array of non-accessible unit
+     *                identifiers as values. MUST be in JSON format.
+     *
+     *                Example: {"7":[],"22":[6,9]}
+     *
+     * @required      true
+     * @since         1.0.0-dev
+     * @var           bool
+     */
+    'Data' => '',
+
+    /**
+     * @default       -
+     * @description   The permission policy identifier. Could be the internal user
+     *                identifier on the LMS.
+     * @required      true
+     * @since         1.0.0-dev
+     * @var           int
+     */
+    'Id' => 1
+
+  ],
+
+  /**
+   * Permission Policy #3
+   * ------------------------------------------------------------------------------------
+   */
+  [
+
+    /**
+     * @default       -
+     * @description   The permission policy data. It is an indexed array, with accessible
+     *                course identifiers as keys, and an array of non-accessible unit
+     *                identifiers as values. MUST be in JSON format.
+     *
+     *                Example: {"7":[],"22":[6,9]}
+     *
+     * @required      true
+     * @since         1.0.0-dev
+     * @var           bool
+     */
+    'Data' => '{"12":[],"13":[14,15]}',
+
+    /**
+     * @default       -
+     * @description   The permission policy identifier. Could be the internal user
+     *                identifier on the LMS.
+     * @required      true
+     * @since         1.0.0-dev
+     * @var           int
+     */
+    'Id' => 2
+
+  ]
+
+];
