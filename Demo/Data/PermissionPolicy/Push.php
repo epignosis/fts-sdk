95a939858b83a32f509147480a0e74dde69ee9f7
xarhsdev <xarhsdev@outlook.com>
2017-08-04 15:49:12 +0300

    Development

--- Demo/Data/PermissionPolicy/Push.php
+++ Demo/Data/PermissionPolicy/Push.php
@@ -62,44 +62,12 @@ $data = [
      * @since         1.0.0-dev
      * @var           string
      */
-    'Policy' => ''
+    'Policy' => '{}'
 
   ],
 
   /**
    * Permission Policy #3
-   * ------------------------------------------------------------------------------------
-   */
-  [
-
-    /**
-     * @default       -
-     * @description   The permission policy identifier. Could be the internal user
-     *                identifier on the LMS.
-     * @required      true
-     * @since         1.0.0-dev
-     * @var           int
-     */
-    'Id' => 1,
-
-    /**
-     * @default       -
-     * @description   The permission policy. It is an indexed array, with accessible
-     *                course identifiers as keys, and an array of non-accessible unit
-     *                identifiers as values. MUST be in JSON format.
-     *
-     *                Example: {"7":[],"22":[6,9]}
-     *
-     * @required      true
-     * @since         1.0.0-dev
-     * @var           string
-     */
-    'Policy' => null
-
-  ],
-
-  /**
-   * Permission Policy #4
    * ------------------------------------------------------------------------------------
    */
   [
