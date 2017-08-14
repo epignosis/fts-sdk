e896093227e8cd1e64b111900aadc69022607b3a
xarhsdev <xarhsdev@outlook.com>
2017-08-14 19:42:10 +0300

    Development

---
+++ Demo/Data/Document/Search.php
@@ -0,0 +1,30 @@
+<?php
+
+$data = [
+
+  /**
+   * Query
+   * ------------------------------------------------------------------------------------
+   */
+
+
+  'Field' => [
+    'File' => true,
+    'Text' => true,
+    'Title' => true
+  ],
+
+  'Pagination' => [
+    'Limit' => 10,
+    'Offset' => 0
+  ],
+
+  'Query' => [
+    'Text' => 'eFrontPro'
+  ],
+
+  'PermissionPolicy' => [
+    'Id' => null
+  ]
+
+];
