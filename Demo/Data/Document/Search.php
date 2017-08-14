333fc28c4aea27a3433fe0ddfba7cf98540f4a24
xarhsdev <xarhsdev@outlook.com>
2017-08-14 19:46:25 +0300

    Development

--- Demo/Data/Document/Search.php
+++ Demo/Data/Document/Search.php
@@ -3,28 +3,47 @@
 $data = [
 
   /**
-   * Query
+   * Query #1
    * ------------------------------------------------------------------------------------
    */
+  [
 
+    'Field' => [
+      'File' => true,
+      'Text' => true,
+      'Title' => true
+    ],
 
-  'Field' => [
-    'File' => true,
-    'Text' => true,
-    'Title' => true
-  ],
+    'Pagination' => [
+      'Limit' => 10,
+      'Offset' => 0
+    ],
 
-  'Pagination' => [
-    'Limit' => 10,
-    'Offset' => 0
-  ],
+    'Query' => [
+      'Text' => 'eFrontPro'
+    ],
 
-  'Query' => [
-    'Text' => 'eFrontPro'
-  ],
+    /**
+     * @default       []
+     * @description   The permission policy of the search.
+     * @required      false
+     * @since         1.0.0-dev
+     * @var           array
+     */
+    'PermissionPolicy' => [
 
-  'PermissionPolicy' => [
-    'Id' => null
+      /**
+       * @default       null
+       * @description   The permission policy identifier. Could be the internal user
+       *                identifier on the LMS.
+       * @required      false
+       * @since         1.0.0-dev
+       * @var           null|int
+       */
+      'Id' => null
+
+    ]
+
   ]
 
 ];
