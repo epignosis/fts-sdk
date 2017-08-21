aff2e4bf08bc54de2d66ae60db6d512797a4c1e3
xarhsdev <xarhsdev@outlook.com>
2017-08-21 22:00:51 +0300

    Development

--- Demo/Data/Account/Create.php
+++ Demo/Data/Account/Create.php
@@ -18,6 +18,19 @@ $data = [
     'Domain' => 'xdev.com',
 
     /**
+     * @default       null
+     * @description   The search index to assign the account. If index will not be set,
+     *                the service will auto assign the account to the best available
+     *                index, after considering a set of different factors. Available
+     *                indexes are, "Proton" and "Nero". Keep in mind, that setting a
+     *                custom index, can lead to the inability to create the account.
+     * @required      true
+     * @since         1.0.0-dev
+     * @var           null|string
+     */
+    'Index' => "Nero",
+
+    /**
      * @default       1
      * @description   The status of the account. Use 1 to be "Enabled" or 0 to be
      *                "Disabled".
@@ -44,6 +57,19 @@ $data = [
      * @var           bool
      */
     'Domain' => 'panagop.talentlms.com',
+
+    /**
+     * @default       null
+     * @description   The search index to assign the account. If index will not be set,
+     *                the service will auto assign the account to the best available
+     *                index, after considering a set of different factors. Available
+     *                indexes are, "Proton" and "Nero". Keep in mind, that setting a
+     *                custom index, can lead to the inability to create the account.
+     * @required      true
+     * @since         1.0.0-dev
+     * @var           null|string
+     */
+    'Index' => null,
 
     /**
      * @default       1
