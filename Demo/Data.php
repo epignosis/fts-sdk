82ac7c08a150a8a93e0c928d0ddc15dadc6c2fda
xarhsdev <xarhsdev@outlook.com>
2017-08-29 09:41:30 +0300

    Development

--- Demo/Data.php
+++ Demo/Data.php
@@ -3,15 +3,56 @@
 /**
  * For more information: https://github.com/epignosis/fts/wiki/Service-API
  */
-require 'Account' . \DIRECTORY_SEPARATOR . 'Create' . \DIRECTORY_SEPARATOR . 'Single.php';
 
-require 'Account' . \DIRECTORY_SEPARATOR . 'Create' . \DIRECTORY_SEPARATOR . 'Single.php';
-require 'Account' . \DIRECTORY_SEPARATOR . 'Create' . \DIRECTORY_SEPARATOR . 'Single.php';
-require 'Account' . \DIRECTORY_SEPARATOR . 'Create' . \DIRECTORY_SEPARATOR . 'Single.php';
-require 'Account' . \DIRECTORY_SEPARATOR . 'Create' . \DIRECTORY_SEPARATOR . 'Single.php';
-require 'Account' . \DIRECTORY_SEPARATOR . 'Create' . \DIRECTORY_SEPARATOR . 'Single.php';
-require 'Account' . \DIRECTORY_SEPARATOR . 'Create' . \DIRECTORY_SEPARATOR . 'Single.php';
-require 'Account' . \DIRECTORY_SEPARATOR . 'Create' . \DIRECTORY_SEPARATOR . 'Single.php';
-require 'Account' . \DIRECTORY_SEPARATOR . 'Create' . \DIRECTORY_SEPARATOR . 'Single.php';
-require 'Account' . \DIRECTORY_SEPARATOR . 'Create' . \DIRECTORY_SEPARATOR . 'Single.php';
-require 'Account' . \DIRECTORY_SEPARATOR . 'Create' . \DIRECTORY_SEPARATOR . 'Single.php';
+// Account:
+require
+  'Account' . \DIRECTORY_SEPARATOR .
+  'Create' . \DIRECTORY_SEPARATOR .
+  'Single.php';
+
+// Document:
+require
+  'Document' . \DIRECTORY_SEPARATOR .
+  'DeIndex' . \DIRECTORY_SEPARATOR .
+  'Multiple.php';
+
+require
+  'Document' . \DIRECTORY_SEPARATOR .
+  'DeIndex' . \DIRECTORY_SEPARATOR .
+  'Single.php';
+
+require
+  'Document' . \DIRECTORY_SEPARATOR .
+  'Index' . \DIRECTORY_SEPARATOR .
+  'Multiple.php';
+
+require
+  'Document' . \DIRECTORY_SEPARATOR .
+  'Index' . \DIRECTORY_SEPARATOR .
+  'Single.php';
+
+require
+  'Document' . \DIRECTORY_SEPARATOR .
+  'Search' . \DIRECTORY_SEPARATOR .
+  'Multiple.php';
+
+// Permission Policy:
+require
+  'PermissionPolicy' . \DIRECTORY_SEPARATOR .
+  'Delete' . \DIRECTORY_SEPARATOR .
+  'Multiple.php';
+
+require
+  'PermissionPolicy' . \DIRECTORY_SEPARATOR .
+  'Delete' . \DIRECTORY_SEPARATOR .
+  'Single.php';
+
+require
+  'PermissionPolicy' . \DIRECTORY_SEPARATOR .
+  'Push' . \DIRECTORY_SEPARATOR .
+  'Multiple.php';
+
+require
+  'PermissionPolicy' . \DIRECTORY_SEPARATOR .
+  'Push' . \DIRECTORY_SEPARATOR .
+  'Single.php';
