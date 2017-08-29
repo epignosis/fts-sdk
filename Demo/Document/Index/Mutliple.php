adf57831e6877f1fdf2d0589efaf23c7abdd581c
xarhsdev <xarhsdev@outlook.com>
2017-08-29 09:38:09 +0300

    Development

--- Demo/Document/Index/Mutliple.php
+++ Demo/Document/Index/Mutliple.php
@@ -1,1 +1,95 @@
 <?php
+
+/**
+ * For more information: https://github.com/epignosis/fts/wiki/Document
+ */
+$data['Document']['Index']['Multiple'] = [
+  // Invalid:
+  [
+
+  ],
+  // Invalid:
+  [
+    'Content' => [
+      'FileList' => [],
+      'TagList' => ['foo', 'bar'],
+      'Text' => 'This is a sample plain text content ..'
+    ],
+  ],
+  // Invalid:
+  [
+    'Content' => [
+      'FileList' => [],
+      'TagList' => ['foo', 'bar'],
+      'Text' => 'This is a sample plain text content ..'
+    ],
+    'Id' => [
+      'Course' => 1,
+      'Lesson' => 2,
+      'Unit' => 3
+    ],
+  ],
+  // Invalid:
+  [
+    'Content' => [
+      'FileList' => [],
+      'TagList' => ['foo', 'bar'],
+      'Text' => 'This is a sample plain text content ..'
+    ],
+    'Title' => 'Plain Text Document'
+  ],
+  // Invalid:
+  [
+    'Content' => [
+      'FileList' => [],
+      'TagList' => ['foo', 'bar'],
+      'Text' => 'This is a sample plain text content ..'
+    ],
+    'Id' => [
+      'Lesson' => 2,
+      'Unit' => 3
+    ],
+    'Title' => 'Plain Text Document'
+  ],
+  // Invalid:
+  [
+    'Content' => [
+      'FileList' => [],
+      'TagList' => ['foo', 'bar'],
+      'Text' => 'This is a sample plain text content ..'
+    ],
+    'Id' => [
+      'Course' => 1,
+      'Lesson' => 2
+    ],
+    'Title' => 'Plain Text Document'
+  ],
+  // Valid:
+  [
+    'Content' => [
+      'FileList' => [],
+      'TagList' => ['foo', 'bar'],
+      'Text' => 'This is a sample plain text content ..'
+    ],
+    'Id' => [
+      'Course' => 1,
+      'Lesson' => 2,
+      'Unit' => 3
+    ],
+    'Title' => 'Plain Text Document'
+  ],
+  // Valid:
+  [
+    'Content' => [
+      'FileList' => ['https://www.talentlms.com/', 'https://www.talentcards.net/'],
+      'TagList' => ['baz', 'qux'],
+      'Text' => 'This is a sample HTML content ..'
+    ],
+    'Id' => [
+      'Course' => 4,
+      'Lesson' => null,
+      'Unit' => 6
+    ],
+    'Title' => 'HTML Document'
+  ]
+];
--- Demo/Document/Index/Single.php
+++ Demo/Document/Index/Single.php
@@ -1,1 +1,94 @@
 <?php
+
+/**
+ * For more information: https://github.com/epignosis/fts/wiki/Document
+ */
+$data['Document']['Index']['Single'] = [
+  // Invalid:
+  [
+
+  ],
+  // Invalid:
+  [
+    'Content' => [
+      'FileList' => ['https://efrontlearning.com/'],
+      'TagList' => ['blah'],
+      'Text' => 'Κείμενο στα Ελληνικά. Yay!'
+    ]
+  ],
+  // Invalid:
+  [
+    'Content' => [
+      'FileList' => ['https://efrontlearning.com/'],
+      'TagList' => ['blah'],
+      'Text' => 'Κείμενο στα Ελληνικά. Yay!'
+    ],
+    'Id' => [
+      'Course' => 65536,
+      'Lesson' => 4096,
+      'Unit' => 1024
+    ],
+  ],
+  // Invalid:
+  [
+    'Content' => [
+      'FileList' => ['https://efrontlearning.com/'],
+      'TagList' => ['blah'],
+      'Text' => 'Κείμενο στα Ελληνικά. Yay!'
+    ],
+    'Title' => 'Google Adwords'
+  ],
+  // Invalid:
+  [
+    'Content' => [
+      'FileList' => ['https://efrontlearning.com/'],
+      'TagList' => ['blah'],
+      'Text' => 'Κείμενο στα Ελληνικά. Yay!'
+    ],
+    'Id' => [
+      'Lesson' => 4096,
+      'Unit' => 1024
+    ],
+    'Title' => 'Google Adwords'
+  ],
+  // Invalid:
+  [
+    'Content' => [
+      'FileList' => ['https://efrontlearning.com/'],
+      'TagList' => ['blah'],
+      'Text' => 'Κείμενο στα Ελληνικά. Yay!'
+    ],
+    'Id' => [
+      'Course' => 65536,
+      'Lesson' => 4096
+    ],
+    'Title' => 'Google Adwords'
+  ],
+  // Valid:
+  [
+    'Content' => [
+      'FileList' => ['https://efrontlearning.com/'],
+      'TagList' => ['blah'],
+      'Text' => 'Κείμενο στα Ελληνικά. Yay!'
+    ],
+    'Id' => [
+      'Course' => 65536,
+      'Lesson' => 4096,
+      'Unit' => 1024
+    ],
+    'Title' => 'Google Adwords'
+  ],
+  // Valid:
+  [
+    'Content' => [
+      'FileList' => [],
+      'TagList' => ['talentlms', 'epignosis'],
+      'Text' => 'Text? No Text ..'
+    ],
+    'Id' => [
+      'Course' => 52,
+      'Unit' => 666
+    ],
+    'Title' => 'Information Security'
+  ]
+];
